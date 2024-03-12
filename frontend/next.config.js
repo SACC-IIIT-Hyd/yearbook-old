b7428cd9bb35fe80e077182b21886bd704fcfea5
Kriti1908 <guptakriti2189@gmail.com>
2024-03-12 17:36:55 +0530

    added photowall components

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
             },
         ]
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com', 
+                port: '',
+                pathname: '/photos/**',
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
