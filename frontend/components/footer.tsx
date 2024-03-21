65c346627580b339c0d581a73bf53c74f73b9047
Amey Karan <ameykaran2005@gmail.com>
2024-03-22 02:35:51 +0530

    Added Shubham's navbar and footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,44 +1,36 @@
-import SocialIcon from "./social-icons";
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import Box from '@mui/material/Box';
 
-var variablesStore = {
-  author: "SACC",
-  title: "Yearbook",
-  email: "sacc@iiit.ac.in",
-  github: "https://github.com/SACC-IIIT-Hyd/",
-  facebook: "https://www.facebook.com/iiith.alumnicell/",
-  linkedin: "https://www.linkedin.com/company/alumni-cell-iiit-h/",
-  instagram: "https://www.instagram.com/alumnicell_iiith/",
-  youtube: "https://www.youtube.com/",
-};
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-export default function Footer() {
+import { Stack, stackClasses } from '@mui/material';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+const footer = () => {
   return (
-    <footer className="flex justify-center">
-      <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
-          <SocialIcon
-            kind="mail"
-            href={`mailto:${variablesStore.email}`}
-            size={6}
-          />
-          <SocialIcon kind="github" href={variablesStore.github} size={6} />
-          <SocialIcon kind="facebook" href={variablesStore.facebook} size={6} />
-          <SocialIcon kind="youtube" href={variablesStore.youtube} size={6} />
-          <SocialIcon kind="linkedin" href={variablesStore.linkedin} size={6} />
-          <SocialIcon
-            kind="instagram"
-            href={variablesStore.instagram}
-            size={6}
-          />
-        </div>
-        <div className="mb-8 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>
-            {variablesStore.author} {variablesStore.title}
-          </div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-        </div>
-      </div>
-    </footer>
-  );
+    <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', backgroundColor: '#0b1120', borderTop: '1px solid #1565c0', padding: '2rem' }}>
+      <Stack>
+        <img src="/logo.png" alt="" />
+      </Stack>
+      <Stack alignItems='center' justifyContent='center' width='30%'>
+        <Typography color='white' variant='h3' gutterBottom>About SACC</Typography>
+        <Typography color='white' variant='subtitle1' textAlign='center' gutterBottom>Student Alumni Connect Cell (SACC) is a student-run organization that aims to foster connections between students, alumni, and the Institute. SACC provides mentorship, organizes events, and offers resources to help students and alumni engage with their alma mater and fellow alumni.</Typography>
+
+      </Stack>
+      <Stack direction='row' gap='1rem'>
+        <FacebookIcon color='primary' fontSize='large'></FacebookIcon>
+        <InstagramIcon color='primary' fontSize='large'></InstagramIcon>
+        <LinkedInIcon color='primary' fontSize='large'></LinkedInIcon>
+        <EmailIcon color='primary' fontSize='large'></EmailIcon>
+      </Stack>
+
+    </Box>
+  )
 }
+
+export default footer
