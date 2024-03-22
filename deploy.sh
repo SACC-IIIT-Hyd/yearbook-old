0f3c70d5c819862286def58325795d6527ac23a9
Bhav Beri <43399374+bhavberi@users.noreply.github.com>
2024-03-22 10:55:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+docker compose -p yearbook -f docker-compose.prod.yml down
 docker compose -p yearbook -f docker-compose.prod.yml up --build -d
+/var/proxy_container.sh -c yearbook-nginx-1 -d yearbook-sacc.iiit.ac.in -p 80
